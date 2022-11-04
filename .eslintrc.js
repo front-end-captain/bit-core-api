3f14b9c05a60116ac45c55780d8224a23ca5dc61
front-end-captain <18810940932@163.com>
2022-11-04 14:58:15 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parserOptions: { tsconfigRootDir: __dirname, project: "./tsconfig.json" },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["*.d.ts", "dist/**"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:promise/recommended",
+    "plugin:import/errors",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  rules: {
+    "import/no-cycle": ["error", { maxDepth: 6 }],
+    "import/no-unresolved": "off",
+    // "import/default": "off",
+    // "import/namespace": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-empty-function": "warn",
+    "@typescript-eslint/ban-types": "warn",
+    "@typescript-eslint/no-var-requires": "warn",
+    "no-console": "error",
+    "no-debugger": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
+};
